ab6a4b97680b888ac62e8d2602ccaf94015dc743
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2022-03-16 18:35:12 +0530

    Print BT in Level order in single line

---
+++ Binary Tree/BTLevelorderSingleLine.cpp
@@ -0,0 +1,21 @@
+void PrintLevelOrder(BTNode* root)
+{
+    queue<BTNode*> q;
+    q.push(root);
+    
+    while(!q.empty())
+    {
+        BTNode* temp = q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+        
+        if(temp->left)
+            q.push(temp->left);
+        
+        if(temp->right)
+            q.push(temp->right);
+     
+    }
+    
+    cout<<endl<<endl;
+}
